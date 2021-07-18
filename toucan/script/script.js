317c7606aa3ffaebc9b821e2d9928c294ce4170e
kombajner <matej.rupnik02@gmail.com>
2021-07-18 12:38:04 +0200

    fixnu to k ni delal pr mapi toucan pac da zarad git folderja ni prikzaval je

---
+++ toucan/script/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.getElementById('hamburger');
+const navbarul = document.getElementById('overlay');
+
+hamburger.addEventListener('click', () => {
+    navbarul.classList.toggle('show');
+});
+
+
+const hamburger2 = document.getElementById('hamburger2');
+const navbarul2 = document.getElementById('overlay');
+
+hamburger2.addEventListener('click', () => {
+    navbarul2.classList.toggle('show');
+});
+
+
+
+
